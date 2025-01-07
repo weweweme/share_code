0cef4fb44f6a025ffbe18ce0716696285d869a4d
weweweme <argentdarae@gmail.com>
2025-01-07 19:47:34 +0900

    feat: LanguageManager 구현

---
+++ ShareCode/ShareCode/src/System/LanguageManager.cs
@@ -0,0 +1,19 @@
+﻿namespace ShareCode.src.System
+{
+    public class LanguageManager
+    {
+        // 언어별 확장자 매핑
+        private readonly Dictionary<ELanguage, List<string>> languageExtensions = new()
+        {
+            { ELanguage.CSharp, new List<string> { ".cs" } },
+            { ELanguage.Java, new List<string> { ".java", ".class", ".jar" } },
+            { ELanguage.C, new List<string> { ".c", ".h" } },
+            { ELanguage.CPP, new List<string> { ".cpp", ".h", ".hpp" } },
+            { ELanguage.Assembly, new List<string> { ".asm", ".s" } }
+        };
+
+        // 현재 선택된 언어
+        private ELanguage _selectedLanguage = ELanguage.CSharp; // 기본값
+        public ELanguage SelectedLanguage => _selectedLanguage;
+    }
+}

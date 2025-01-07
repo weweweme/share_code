ba221babe608e9622c9485477063216f8b81c728
weweweme <argentdarae@gmail.com>
2025-01-07 15:58:37 +0900

    chore: C# 프로젝트 셋업

---
+++ ShareCode/ShareCode/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShareCode
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

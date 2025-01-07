3e1f5280b67d3a1e1648a052a0cc3b61eac5d323
weweweme <argentdarae@gmail.com>
2025-01-07 17:11:58 +0900

    feat: 창 레이아웃 셋업

--- ShareCode/ShareCode/MainPage.xaml.cs
+++ ShareCode/ShareCode/MainPage.xaml.cs
@@ -1,25 +1,45 @@
-﻿namespace ShareCode
+﻿namespace ShareCode;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+        // 버튼 클릭 이벤트 핸들러 설정
+        var openButton = this.FindByName<Button>("Open");
+        var exportButton = this.FindByName<Button>("Export");
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+        openButton.Clicked += OnOpenButtonClicked;
+        exportButton.Clicked += OnExportButtonClicked;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        // Export 버튼은 초기에는 비활성화 상태
+        exportButton.IsEnabled = false;
     }
 
+    // Open 버튼 클릭 이벤트 핸들러
+    private void OnOpenButtonClicked(object? sender, EventArgs e)
+    {
+        if (sender == null)
+        { 
+            // TODO: 추가 처리
+            return; 
+        }
+
+        // TODO: Open 버튼 클릭 동작 구현 (폴더 선택 등)
+        Console.WriteLine("Open 버튼 클릭");
+    }
+
+    // Export 버튼 클릭 이벤트 핸들러
+    private void OnExportButtonClicked(object? sender, EventArgs e)
+    {
+        if (sender == null)
+        {
+            // TODO: 추가 처리
+            return;
+        }
+
+        // TODO: Export 버튼 클릭 동작 구현 (파일 내보내기 등)
+        Console.WriteLine("Export 버튼 클릭");
+    }
 }
--- ShareCode/ShareCode/obj/Debug/net8.0-ios/iossimulator-x64/ShareCode.AssemblyInfo.cs
+++ ShareCode/ShareCode/obj/Debug/net8.0-ios/iossimulator-x64/ShareCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3635703f5849cc41c6e4e480a8225cb74047a0a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+023a8acc4e4f93aa6c3137ec4d9e6aa1bda9cb92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

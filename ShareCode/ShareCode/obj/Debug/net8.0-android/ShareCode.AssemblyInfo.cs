023a8acc4e4f93aa6c3137ec4d9e6aa1bda9cb92
weweweme <argentdarae@gmail.com>
2025-01-07 16:27:51 +0900

    chore: Android Build SDK 설치

--- ShareCode/ShareCode/obj/Debug/net8.0-android/ShareCode.AssemblyInfo.cs
+++ ShareCode/ShareCode/obj/Debug/net8.0-android/ShareCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ba221babe608e9622c9485477063216f8b81c728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3635703f5849cc41c6e4e480a8225cb74047a0a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ShareCode/ShareCode/obj/Debug/net8.0-ios/iossimulator-x64/ShareCode.AssemblyInfo.cs
+++ ShareCode/ShareCode/obj/Debug/net8.0-ios/iossimulator-x64/ShareCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7b509df5f87a8eff92138eda4006e6c0e7c63041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3635703f5849cc41c6e4e480a8225cb74047a0a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShareCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

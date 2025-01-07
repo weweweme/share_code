6b5b77e9ab7189ca3128e3621f07b5a4b5f8d757
weweweme <argentdarae@gmail.com>
2025-01-07 19:26:11 +0900

    feat: Window에서 파일을 오픈하는 기능 추가

--- ShareCode/ShareCode/MainPage.xaml.cs
+++ ShareCode/ShareCode/MainPage.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace ShareCode;
+﻿using CommunityToolkit.Maui.Storage;
+using System.Collections.ObjectModel;
+
+namespace ShareCode;
 
 public partial class MainPage : ContentPage
 {
+    private ObservableCollection<string> fileList = new();
+
     public MainPage()
     {
         InitializeComponent();
+
+        // 파일 목록 바인딩
+        FileListView.ItemsSource = fileList;
 
         // 버튼 클릭 이벤트 핸들러 설정
         var openButton = this.FindByName<Button>("Open");
@@ -18,12 +26,37 @@ public partial class MainPage : ContentPage
     }
 
     // Open 버튼 클릭 이벤트 핸들러
-    private void OnOpenButtonClicked(object? sender, EventArgs e)
+    private async void OnOpenButtonClicked(object? sender, EventArgs e)
     {
         if (sender == null)
         { 
             // TODO: 추가 처리
             return; 
+        }
+
+        if (OperatingSystem.IsWindows())
+        {
+            FolderPickerResult folder = await FolderPicker.PickAsync(default);
+
+            if (folder != null)
+            {
+                // 선택된 폴더의 경로
+                var folderPath = folder.Folder.Path;
+
+                // 파일 목록을 가져와 ObservableCollection에 추가
+                string[] files = Directory.GetFiles(folderPath);
+                fileList.Clear();
+                foreach (var file in files)
+                {
+                    fileList.Add(Path.GetFileName(file)); // 파일명만 추가
+                }
+
+                // Export 버튼 활성화
+                var exportButton = this.FindByName<Button>("Export");
+                exportButton.IsEnabled = fileList.Count > 0;
+
+                Console.WriteLine($"선택된 폴더: {folderPath}");
+            }
         }
 
         // TODO: Open 버튼 클릭 동작 구현 (폴더 선택 등)

e61db3827de2055933cc77ec166afac75acf5602
weweweme <argentdarae@gmail.com>
2025-01-07 22:33:57 +0900

    feat: 맥용 OnOpenButtonClicked 구현

--- ShareCode/ShareCode/src/Platform/MacOSPlatformHandler.cs
+++ ShareCode/ShareCode/src/Platform/MacOSPlatformHandler.cs
@@ -4,12 +4,41 @@ namespace ShareCode.src.Platform
 {
     public class MacOSPlatformHandler : PlatformHandler
     {
-        public override Task OnOpenButtonClicked(ContentPage page, ObservableCollection<FileItem> originFileList, ObservableCollection<FileItem> fileList)
+        public override async Task OnOpenButtonClicked(ContentPage page, ObservableCollection<FileItem> originFileList, ObservableCollection<FileItem> uiFileList)
         {
-            throw new NotImplementedException();
+            // macOS에서는 FolderPicker 대신 FilePicker 사용
+            var folderResult = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "폴더를 선택하세요."
+            });
+
+            if (folderResult != null)
+            {
+                var folderPath = folderResult.FullPath;
+                string[] files = Directory.GetFiles(folderPath);
+                uiFileList.Clear();
+                originFileList.Clear();
+
+                foreach (var file in files)
+                {
+                    var fileItem = new FileItem(Path.GetFileName(file), file);
+                    fileItem.IsChecked = false;
+                    originFileList.Add(fileItem);
+                }
+
+                FileFilter.FilterAndUpdateUI(LanguageManager.SelectedLanguage, originFileList, uiFileList);
+
+                var exportButton = page.FindByName<Button>(GlobalConstants.EXPORT);
+                exportButton.IsEnabled = uiFileList.Count > 0;
+
+                if (LanguageManager.SelectedLanguage == ELanguage.None)
+                {
+                    exportButton.IsEnabled = false;
+                }
+            }
         }
 
-        public override Task OnExportButtonClicked(ContentPage page, ObservableCollection<FileItem> fileList)
+        public override async Task OnExportButtonClicked(ContentPage page, ObservableCollection<FileItem> fileList)
         {
             throw new NotImplementedException();
         }
--- ShareCode/ShareCode/src/Platform/PlatformHandler.cs
+++ ShareCode/ShareCode/src/Platform/PlatformHandler.cs
@@ -4,7 +4,7 @@ namespace ShareCode.src.Platform
 {
     public abstract class PlatformHandler
     {
-        public abstract Task OnOpenButtonClicked(ContentPage page, ObservableCollection<FileItem> originFileList, ObservableCollection<FileItem> fileList);
+        public abstract Task OnOpenButtonClicked(ContentPage page, ObservableCollection<FileItem> originFileList, ObservableCollection<FileItem> uiFileList);
         public abstract Task OnExportButtonClicked(ContentPage page, ObservableCollection<FileItem> fileList);
     }
 }
